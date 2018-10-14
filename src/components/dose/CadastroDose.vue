69947780294918da310fcb1365fdbb0c09a9c0f9
Brunno Henrique <brunnoh11@gmail.com>
2018-10-13 22:21:36 -0300

    Adicionando telas de cadastro e lista de Dose (vacinação). OBS: não testado! ainda concluir o back!

--- src/components/dose/CadastroDose.vue
+++ src/components/dose/CadastroDose.vue
@@ -26,40 +26,95 @@
               <span class='title'>Informações gerais</span>
             </v-flex>
 
-            <v-flex xs12 sm6 d-flex>
-              <v-select
-                :items="items.animal"
-                box
-                label="Selecionar Animal"
-              ></v-select>
-            </v-flex>
-            <v-flex xs12 sm6 d-flex>
-              <v-select
-                :items="items.medicamento"
-                box
-                label="Selecionar Medicamento"
-              ></v-select>
+            <!--Adicionar Animal-->
+            <v-flex xs12 sm6 md6 lg6>
+              <v-combobox
+                v-model="dose.animal"
+                :items="selectAnimal.items"
+                item-text="nome"
+                item-value="id"
+                label="Animal"
+                placeholder=""
+              ></v-combobox>
             </v-flex>
 
+            <!--Adicionar Medicamento-->
+            <v-flex xs12 sm4 md4 lg4>
+              <v-combobox
+                v-model="dose.medicamento"
+                :items="selectMedicamento.items"
+                item-text="nome"
+                item-value="id"
+                label="Medicamento"
+                placeholder=""
+              ></v-combobox>
+            </v-flex>
+
+            <!--Cadastrar Medicamento-->
+            <v-flex xs12 sm2 md2 lg2>
+              <v-layout row justify-center>
+                <v-dialog v-model="dialog" persistent max-width="500px">
+                  <v-btn fab slot="activator" color="indigo" dark><v-icon dark>add</v-icon></v-btn>
+                  <v-card>
+                      <v-flex xs12>
+                        <v-alert
+                          v-if="alerterForm.estado"
+                          :type="alerterForm.tipo"
+                        >
+                          {{alerterForm.mensagem}}
+                        </v-alert>
+                        <span class='title'>Cadastrar Medicamento</span>
+                      </v-flex>
+                      <v-card-title>
+
+                    </v-card-title>
+                    <v-card-text>
+                      <v-container grid-list-md>
+                        <v-layout wrap>
+                          <v-flex xs12 sm6 md6>
+                            <v-text-field
+                              v-model="formMedicamento.nome"
+                              label="Nome"
+                              required
+                            >
+                            </v-text-field>
+                          </v-flex>
+                          <v-flex xs12 sm6 md6>
+                            <v-text-field
+                              v-model="formMedicamento.prescricao"
+                              label="Prescricao"
+                            >
+                            </v-text-field>
+                          </v-flex>
+                        </v-layout>
+                      </v-container>
+                      <small>*Preencha todos os campos corretamente</small>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-spacer></v-spacer>
+
+                      <v-btn color="blue darken-1" flat @click="cadastrarMedicamento">Salvar</v-btn>
+                      <v-btn color="blue darken-1" flat @click="clearFormMedicamento">Limpar Formulário</v-btn>
+                      <v-btn color="blue darken-1" flat @click.native="dialog = false">Cancelar</v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-layout>
+            </v-flex>
 
             <v-flex xs12 sm6 md6 lg6>
               <v-text-field
-                v-model="hemograma.ppt"
-                label='PPT'
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm6 md6 lg6>
-              <v-text-field
-                v-model="hemograma.hematocrito"
-                label='Hematocrito'
+                v-model="dose.quantidadeMg"
+                label='Dose'
+                placeholder="Medida em miligramas"
               ></v-text-field>
             </v-flex>
           </v-layout>
           <v-flex>
-            <v-btn v-if="!hemograma.id" @click="cadastrar">Enviar</v-btn>
-            <v-btn v-if="hemograma.id" @click="editar">Editar</v-btn>
+            <v-btn v-if="!dose.id" @click="cadastrar">Enviar</v-btn>
+            <v-btn v-if="dose.id" @click="editar">Editar</v-btn>
 
-            <v-btn @click="clear">Limpar formulário</v-btn>
+            <v-btn @click="clearForm">Limpar formulário</v-btn>
           </v-flex>
         </v-form>
       </v-card-text>
@@ -68,51 +123,147 @@
 </template>
 
 <script>
-    export default {
-      name: 'cadastro-dose',
-      data() {
-        return {
-          dose: {
-            id: null,
-            quantidadeMg: ''
-          },
-          items: {
-            medicamento: [],
-            animal: []
-          },
-          alerter: {
-            tipo: '',
-            estado: true,
-            mensagem: ''
-          },
-          nomeTitulo: 'Cadastro de Vacina'
+  import DosesService from '../../services/DosesService'
+  import {AnimaisService} from '../../services/AnimaisService'
+  import MedicamentosService from '../../services/MedicamentosService'
+  export default {
+    name: 'cadastro-dose',
+    data() {
+      return {
+        dose: {
+          id: null,
+          quantidadeMg: '',
+          animal: [],
+          medicamento:[]
+        },
+        formMedicamento: {
+          id: null,
+          nome: '',
+          prescricao: ''
+        },
+        selectAnimal: {
+          items: []
+        },
+        selectMedicamento: {
+          items: []
+        },
+        alerter: {
+          tipo: '',
+          estado: false,
+          mensagem: ''
+        },
+        alerterForm: {
+          tipo: '',
+          estado: false,
+          mensagem: ''
+        },
+        nomeTitulo: 'Vacinar Animal',
+        dialog: false
+      }
+    },
+    mounted() {
+      this.getAnimais()
+      this.getMedicamentos()
+      this.dose.id = this.$route.params.id
+      if (this.dose.id) {
+        this.nomeTitulo = 'Editar Vacina'
+        this.getDose()
+      }
+    },
+    methods: {
+      async getDose() {
+        let response = await DosesService._getById(this.dose.id)
+        this.dose = response.data.doses
+      },
+      async getAnimais() {
+        let response = await AnimaisService._getAll(this.dose.animal)
+        this.selectAnimal.items = response.data.animais.data
+      },
+      async getMedicamentos() {
+        let response = await MedicamentosService._getAll(this.dose.medicamento)
+        this.selectMedicamento.items = response.data.medicamentos.data
+      },
+      async cadastrarMedicamento() {
+
+        if (this.validarFormMedicamento()) {
+          console.log('aqui');
+          // let response = await MedicamentosService._create(this.formMedicamento)
+          // if (response.status === 200) {
+          //   this.alerta(response.data.message.type, true, response.data.message.description)
+          //
+          // }
+          // if (response.status === 400) {
+          //   this.alerta(response.data.message.type, true, response.data.message)
+          // }
+          // if (response.status === 500) {
+          //   this.alerta(response.data.message.type, true, response.data.message)
+          // }
+          // this.dialog = false
+        } else {
+          this.alertaForm('warning', true, 'Preencha os campos corretamente!')
         }
       },
-      mounted() {
-        this.dose.id = this.$route.params.id
-        if (this.dose.id) {
-          this.nomeTitulo = 'Editar Cadastro de Vacina'
+      async cadastrar() {
+        console.log(this.dose)
+        // let response = await DosesService._create(this.dose)
+        // if(response.status === 200) {
+        //
+        // }
+        // if(response.status === 200) {
+        //
+        // }
+        // if(response.status === 200) {
+        //
+        // }
+      },
+      async editar() {
+        let response = await DosesService._create(this.dose)
+        if(response.status === 200) {
+
+        }
+        if(response.status === 200) {
+
+        }
+        if(response.status === 200) {
 
         }
       },
-      methods: {
-        async getDose() {
-
-        },
-        async getAnimais() {
-
-        },
-        async getMedicamentos() {
-
-        },
-        async cadastrar() {
-
-        },
-        async editar() {
-
+      validarFormDose() {
+        if (this.dose.animal.id !== '' && this.dose.animal.id !== null &&
+            this.dose.medicamento.id !== '' && this.dose.medicamento.id !== null &&
+            this.dose.quantidadeMg !== '' && this.dose.quantidadeMg !== null) {
+          return true
+        } else {
+          return false
         }
+      },
+      validarFormMedicamento() {
+        console.log('aqui');
+        if (this.formMedicamento.nome !== '' && this.formMedicamento.nome !== null &&
+          this.formMedicamento.prescricao !== '' && this.formMedicamento.prescricao !== null) {
+          return true
+        }
+        return false
+      },
+      clearForm() {
+
+      },
+      clearFormMedicamento() {
+        this.formMedicamento.nome = ''
+        this.formMedicamento.prescricao = ''
+      },
+      alerta(tipo, estado, mensagem){
+        this.alerter.tipo = tipo
+        this.alerter.estado = estado
+        this.alerter.mensagem = mensagem
+      },
+      alertaForm(tipo, estado, mensagem){
+        this.alerterForm.tipo = tipo
+        this.alerterForm.estado = estado
+        this.alerterForm.mensagem = mensagem
       }
     }
+  }
 </script>
 
 <style scoped>
--- src/components/dose/ListagemDose.vue
+++ src/components/dose/ListagemDose.vue
@@ -1,11 +1,147 @@
 <template>
+  <v-container grid-list-md text-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
 
+          <!--Cabeçalho do componente-->
+          <v-card-title primary-title>
+            <div>
+              <h2 class="title mb-0">Pesquisa</h2>
+              <span class="caption">Faça uma pesquisa utilizando os filtros abaixo para um melhor resultado.</span>
+            </div>
+          </v-card-title>
+
+          <!--Sessão de formulário de pesquisa-->
+          <v-card-text>
+            <v-form>
+              <v-layout row wrap>
+
+                <v-flex xs12 md2 lg2>
+                  <v-text-field
+                    label="Buscar pelo Id"
+                    v-model="buscaDose.id"
+                  />
+                </v-flex>
+                <!-- <v-flex xs12 md4 lg4>
+                  <v-text-field xs12 md3
+                    label="Buscar pelo nome"
+                    v-model="buscaCargo.nome"
+                  />
+                </v-flex> -->
+              </v-layout>
+              <v-btn color="success" v-on:click="getDoses">Buscar!</v-btn>
+              <v-btn color="secondary" v-on:click="clear">Redefinir busca</v-btn>
+            </v-form>
+          </v-card-text>
+
+          <!--Tabela de apresentação de dados-->
+
+          <v-flex xs12>
+            <v-card>
+              <v-card-text>
+                <v-data-table
+                  :headers="headers"
+                  :items="items.data"
+                  hide-actions
+                >
+                  <template slot="items" slot-scope="props">
+                    <tr>
+                      <td class="text-xs-center">{{ props.item.id }}</td>
+                      <td class="text-xs-center">{{ props.item.animal.nome }}</td>
+                      <td class="text-xs-center">{{ props.item.medicamento.nome }}</td>
+                      <td class="text-xs-center">{{ props.item.quantidadeMg }}</td>
+
+                      <td class="justify-center layout px-0">
+
+                        <!--Icone de editar-->
+                        <v-icon
+                          small
+                          class="mr-2"
+                          @click="editar(props.item.id)"
+                        >
+                          edit
+                        </v-icon>
+
+                        <!--Icone de deletar-->
+                        <v-icon
+                          small
+                          @click="deletar(props.item.id)"
+                        >
+                          delete
+                        </v-icon>
+                      </td>
+                    </tr>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+              <v-card-actions v-if="items.length !== 0" class="text-xs-center">
+                <v-layout>
+                  <v-flex xs12>
+                    <v-pagination :length="items.last_page" v-model="buscaCargo.params.pagina" @input="getCargos"/>
+                  </v-flex>
+                </v-layout>
+
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: "ListagemDose"
+  import DosesService from '../../services/DosesService'
+  export default {
+    name: 'listagem-dose',
+    data() {
+      return {
+        items: [],
+        loading: false,
+        isLoading: false,
+        buscaDose: {
+          id: null,
+          medicamento: {
+            id: null,
+            nome: ''
+          },
+          animal: {
+            id: null,
+            nome: null
+          },
+          params: {
+            pagina: 1
+          }
+        },
+        search: null,
+        readers: [
+          {text: 'ID', value: 'id'},
+          {text: 'Medicamento', value: 'medicamento'},
+          {text: 'Animal', value: 'animal'},
+          {text: 'Actions', value: 'name', sortable: false}
+        ]
+      }
+    },
+    mounted() {
+      this.getDoses()
+    },
+    methods: {
+      async getDoses() {
+        let response = await DosesService._getAll(this.buscaDose)
+        this.items = response.data.doses
+      },
+      async deletar(id) {
+        if(confirm('Deseja deletar este item?')){
+          DosesService._delete(id)
+        }
+      },
+      async editar() {
+
+      }
     }
+  }
 </script>
 
 <style scoped>
