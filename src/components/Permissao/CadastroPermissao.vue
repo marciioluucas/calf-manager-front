ee8e5fe7cc1005b876395d10118a4e37180be42a
Brunno Henrique <brunnoh11@gmail.com>
2018-10-28 16:41:42 -0300

    Alterando cadastro de permissão e adicionando tela de listagem

--- src/components/Permissao/CadastroPermissao.vue
+++ src/components/Permissao/CadastroPermissao.vue
@@ -26,22 +26,56 @@
               <span class='title'>Informações gerais</span>
             </v-flex>
 
-            <v-flex xs12 sm6 md6 lg6>
+            <v-flex xs12 sm12 md12>
               <v-text-field
-                v-model="doenca.nome"
-                label='Nome da Doenca'
-              ></v-text-field>
+                v-model="permissao.nome_modulo"
+                label="Nome"
+                required
+              >
+              </v-text-field>
             </v-flex>
-            <v-flex xs12 sm6 md6 lg6>
-              <v-text-field
-                v-model="doenca.descricao"
-                label='Descrição da Doenca'
-              ></v-text-field>
+            <v-flex xs12 sm2 md2>
+              <v-switch
+                v-model="permissao.create"
+                label="Create"
+                color="primary"
+                value='1'
+                hide-details
+              ></v-switch>
             </v-flex>
+
+            <v-flex xs12 sm2 md2>
+              <v-switch
+                v-model="permissao.read"
+                label="Read"
+                color="primary"
+                value='1'
+                hide-details
+              ></v-switch>
+            </v-flex>
+            <v-flex xs12 sm2 md2>
+              <v-switch
+                v-model="permissao.update"
+                label="Update"
+                color="primary"
+                value='1'
+                hide-details
+              ></v-switch>
+            </v-flex>
+            <v-flex xs12 sm2 md2>
+              <v-switch
+                v-model="permissao.delete"
+                label="Delete"
+                color="primary"
+                value='1'
+                hide-details
+              ></v-switch>
+            </v-flex>
+
           </v-layout>
           <v-flex>
-            <v-btn v-if="!doenca.id" @click="cadastrar">Enviar</v-btn>
-            <v-btn v-if="doenca.id" @click="editar">Editar</v-btn>
+            <v-btn v-if="!permissao.id" @click="cadastrar">Enviar</v-btn>
+            <v-btn v-if="permissao.id" @click="editar">Editar</v-btn>
 
             <v-btn @click="clear">Limpar formulário</v-btn>
           </v-flex>
@@ -52,9 +86,70 @@
 </template>
 
 <script>
-    export default {
-        name: "CadastroPermissao"
+  export default {
+    name: "cadastro-permissao",
+    data(){
+      return {
+        permissao: {
+          id: null,
+          nome_modulo: '',
+          create: 0,
+          read: 0,
+          update: 0,
+          delete: 0
+        },
+        alerter: {
+          tipo: 'success',
+          estado: false,
+          mensagem: ''
+        },
+        nomeTitulo: 'Cadastro de Permissão'
+      }
+    },
+    mounted() {
+      if(this.permissao.id = this.$route.params.id){
+        this.nomeTitulo = 'Editar Permissão'
+        this.getPermissao()
+      }
+    },
+    methods: {
+      async getPermissao(){
+        let response = await PermissoesService._getById(this.permissao)
+        this.permissao = response.data.permissoes
+      },
+      async cadastrar(){
+        if (this.validarForm()){
+          let response = await PermissaoService._create(this.permissao)
+          if (response.status === 201){
+            this.alerta(response.data.message.type, true, response.data.message.description)
+          }
+        }
+      },
+      async editar(){
+        if (this.validarForm()){
+          let response = await PermissaoService._update(this.permissao)
+          if (response.status === 201){
+            this.alerta(response.data.message.type, true, response.data.message.description)
+          }
+        }      },
+      validarForm(){
+      if(this.permissao.nome_modulo !== '' && this.permissao.nome_modulo !== null && this.permissao.create !== null &&
+         this.permissao.read !== null && this.permissao.update !== null&& this.permissao.delete !== null) {
+            return true
+          } else {
+            return false
+          }
+      },
+      alerta(tipo, estado, mensagem){
+        this.alerter.tipo = tipo
+        this.alerter.estado = estado
+        this.alerter.mensagem = mensagem
+      },
+      clear(){
+
+      }
     }
+  }
 </script>
 
 <style scoped>
--- src/components/Permissao/ListagemPermissao.vue
+++ src/components/Permissao/ListagemPermissao.vue
@@ -1,11 +1,146 @@
 <template>
+  <v-container grid-list-md text-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card>
 
+          <!--Cabeçalho do componente-->
+          <v-card-title primary-title>
+            <div>
+              <h2 class="title mb-0">Pesquisa</h2>
+              <span class="caption">Faça uma pesquisa utilizando os filtros abaixo para um melhor resultado.</span>
+            </div>
+          </v-card-title>
+
+          <!--Sessão de formulário de pesquisa-->
+          <v-card-text>
+            <v-form>
+              <v-layout row wrap>
+
+                <v-flex xs12 md2 lg2>
+                  <v-text-field
+                    label="Buscar pelo Id"
+                    v-model="buscaPermissao.id"
+                  />
+                </v-flex>
+                <v-flex xs12 md4 lg4>
+                  <v-text-field xs12 md3
+                    label="Buscar pelo nome"
+                    v-model="buscaPermissao.nome"
+                  />
+                </v-flex>
+              </v-layout>
+              <v-btn color="success" v-on:click="getPermissoes">Buscar!</v-btn>
+              <v-btn color="secondary" v-on:click="clear">Redefinir busca</v-btn>
+            </v-form>
+          </v-card-text>
+
+          <!--Tabela de apresentação de dados-->
+
+          <v-flex xs12>
+            <v-card>
+              <v-card-text>
+                <v-data-table
+                  :headers="headers"
+                  :items="items.data"
+                  hide-actions
+                >
+                  <template slot="items" slot-scope="props">
+                    <tr>
+                      <td class="text-xs-center">{{ props.item.id }}</td>
+                      <td class="text-xs-center">{{ props.item.nome_modulo }}</td>
+                      <td class="justify-center layout px-0">
+                        <v-icon
+                          small
+                          class="mr-2"
+                          @click="editar(props.item.id)"
+                        >
+                          edit
+                        </v-icon>
+                        <v-icon
+                          small
+                          @click="deletar(props.item.id)"
+                        >
+                          delete
+                        </v-icon>
+                      </td>
+                    </tr>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+              <v-card-actions v-if="items.length !== 0" class="text-xs-center">
+                <v-layout>
+                  <v-flex xs12>
+                    <v-pagination :length="items.last_page" v-model="buscaPermissao.params.pagina" @input="getPermissoes"/>
+                  </v-flex>
+                </v-layout>
+
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-    export default {
-        name: "ListagemPermissao"
+  import PermissoesService from '../../services/PermissoesService'
+  export default {
+    name: 'listagem-permissao',
+    data() {
+      return {
+        items: [],
+        buscaPermissao: {
+          id: null,
+          nome_modulo: '',
+          params: {
+            pagina: 1
+          }
+        },
+        search: false,
+        headers: [
+          {text: 'ID', value: 'id'},
+          {text: 'Nome do módulo', value: 'nome_modulo'},
+          // {text: 'Create', value: 'create'},
+          // {text: 'Read', value: 'read'},
+          // {text: 'Update', value: 'update'},
+          // {text: 'Delete', value: 'delete'},
+          {text: 'Ações', value: 'acoes'}
+        ]
+      }
+    },
+    mounted(){
+      this.getPermissoes()
+    },
+    methods: {
+      async getPermissoes(){
+        let response = await PermissoesService._getAll(this.buscaPermissao)
+        this.items = response.data.permissoes
+      },
+      async editar(id){
+        this.$router.push({
+          name: 'CadastroPermissao',
+          params: {id: id}
+        })
+      },
+      async deletar(id){
+        if(confirm('Deseja deletar este item?')){
+          await PermissoesService._delete(id)
+        }
+        this.atualizarTabela()
+      },
+      clear(){
+        this.buscaPermissao.id = null,
+        this.buscaPermissao.nome_modulo = ''
+      },
+      atualizarTabela(){
+        this.items = []
+        this.getPermissoes()
+      }
     }
+  }
 </script>
 
 <style scoped>
